18ae3670180cf8c37d9899799bb8c863a5caa53b
Michael Hepfer <michael.hepfer@ibm.com>
2021-02-25 09:35:40 -0600

    changes ip for connection to localhost

--- src/main/java/com/github/michaelsteven/archetype/springboot/webflux/items/model/ItemEntity.java
+++ src/main/java/com/github/michaelsteven/archetype/springboot/webflux/items/model/ItemEntity.java
@@ -1,5 +1,9 @@
 package com.github.michaelsteven.archetype.springboot.webflux.items.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
@@ -11,14 +15,14 @@ import lombok.NoArgsConstructor;
  */
 @Data
 @NoArgsConstructor
-//@Entity
+@Entity
 @Table("items")
 public class ItemEntity extends Auditable {
 
 	/** The id. */
 	@Id
-	//@GeneratedValue(strategy = GenerationType.IDENTITY)
-	//@Column(name = "id", unique = true, nullable = false)
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id", unique = true, nullable = false)
 	private Long id;
 	
 	/** The name. */
